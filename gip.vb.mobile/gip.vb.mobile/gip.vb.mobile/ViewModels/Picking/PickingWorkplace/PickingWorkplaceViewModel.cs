6d5f8134213821560ccab7edaef68e515c0af9f2
Ivan Hrastinski <ihrastinski@kajbum.com>
2022-01-29 08:23:56 +0100

    Picking workplace

---
+++ gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/ViewModels/Picking/PickingWorkplace/PickingWorkplaceViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gip.vb.mobile.ViewModels
+{
+    public class PickingWorkplaceViewModel : PickingViewModel
+    {
+        public PickingWorkplaceViewModel()
+        {
+        }
+    }
+}
---
+++ gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/Views/BSOPicking/BSOPickingWorkplace/BSOPickingWorkplace.xaml.cs
@@ -0,0 +1,42 @@
+﻿using gip.vb.mobile.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace gip.vb.mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BSOPickingWorkplace : BSOPageBase
+    {
+        public BSOPickingWorkplace()
+        {
+            BarcodeScan = new BarcodeScanner();
+            BarcodeScan.IsEnabledInvokeBarcodeOnServer = true;
+
+            InitializeComponent();
+        }
+
+        public BarcodeScanner BarcodeScan
+        {
+            get;
+            set;
+        }
+
+        protected override void OnAppearing()
+        {
+            BarcodeScan.OnAppearing();
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            BarcodeScan.OnDisappearing();
+            base.OnDisappearing();
+        }
+
+    }
+}
