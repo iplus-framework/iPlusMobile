e28b601e62ec9445f7a26981f2103362c68e6fc9
Ivan Hrastinski <ihrastinski@kajbum.com>
2022-04-20 15:58:14 +0200

    manual weighing designs and mobile

---
+++ gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/Views/BSOBarcode/BSOBarcodeTaskManuDetails.xaml.cs
@@ -0,0 +1,181 @@
+﻿using gip.mes.webservices;
+using gip.vb.mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace gip.vb.mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BSOBarcodeTaskManuDetails : BSOPageBase
+    {
+        BarcodeScanManuModel _ViewModel;
+
+        public BSOBarcodeTaskManuDetails(BarcodeScanManuModel viewModel)
+        {
+            _ViewModel = viewModel;
+            InitializeComponent();
+            BindingContext = _ViewModel;
+            EnableButtons();
+        }
+
+        #region Properties
+
+
+        #endregion
+
+
+        #region Methods
+
+        private void BtnReleaseMachine_Clicked(object sender, EventArgs e)
+        {
+            if (!IsEnabledBtnReleaseMachine())
+                return;
+
+            _ViewModel.ReleaseMachine();
+        }
+
+        private void BtnOccupyMachine_Clicked(object sender, EventArgs e)
+        {
+            if (!IsEnabledBtnOccupyMachine())
+                return;
+
+            _ViewModel.InvokeActionOnMachine();
+        }
+
+        private void BtnPauseOrderOnMachine_Clicked(object sender, EventArgs e)
+        {
+            if (!IsEnabledBtnPauseOrderOnMachine())
+                return;
+
+            _ViewModel.PauseOrderOnMachine();
+        }
+
+        private async void BtnDoBooking_Clicked(object sender, EventArgs e)
+        {
+            if (!IsEnabledBtnDoBooking())
+                return;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo.IntermediateBatch != null)
+            {
+                if (!wfInfo.IntermediateBatch.IsFinalMixure)
+                {
+                    await Navigation.PushAsync(new BSOProdOrderOutwardMatSel(wfInfo.IntermediateBatch, _ViewModel));
+                }
+                else if (!wfInfo.IntermediateBatch.HasInputMaterials)
+                {
+                    await Navigation.PushAsync(new BSOProdOrderInward(wfInfo.IntermediateBatch, _ViewModel));
+                }
+                else
+                {
+                    await Navigation.PushAsync(new BSOProdOrderInOutSelector(wfInfo.IntermediateBatch, _ViewModel));
+                }
+            }
+            else if (wfInfo.Intermediate != null)
+            {
+                await Navigation.PushAsync(new BSOProdOrderBatch(wfInfo.Intermediate, _ViewModel));
+            }
+            else
+            {
+                await Navigation.PushAsync(new BSOProdOrderIntermediate(wfInfo.ProdOrderPartslist, _ViewModel));
+            }
+        }
+
+        private async void BtnEnterACMParams_Clicked(object sender, EventArgs e)
+        {
+            if (!IsEnabledBtnEnterACMParams())
+                return;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo == null || wfInfo.WFMethod == null)
+                return;
+            wfInfo.WFMethod.AutoRemove = false;
+            await Navigation.PushAsync(new BSOACMethodEditor(wfInfo.WFMethod));
+        }
+
+        private void SendChangedACMethod()
+        {
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo != null && wfInfo.WFMethod != null && wfInfo.WFMethod.AutoRemove == true)
+            {
+                wfInfo.WFMethod.AutoRemove = false;
+                BarcodeEntity entity = _ViewModel.Item.Sequence.LastOrDefault();
+                if (entity == null)
+                {
+                    _ViewModel.ResetScanSequence();
+                    return;
+                }
+                entity.SelectedOrderWF = wfInfo;
+                entity.WFMethod = wfInfo.WFMethod;
+                _ViewModel.InvokeBarcodeCommand.Execute(null);
+            }
+        }
+
+        private void EnableButtons()
+        {
+            BtnDoBooking.IsEnabled = IsEnabledBtnDoBooking();
+            BtnOccupyMachine.IsVisible = IsEnabledBtnOccupyMachine();
+            BtnReleaseMachine.IsVisible = IsEnabledBtnReleaseMachine();
+            BtnPauseOrderOnMachine.IsEnabled = IsEnabledBtnPauseOrderOnMachine();
+            BtnEnterACMParams.IsEnabled = IsEnabledBtnEnterACMParams();
+        }
+
+        private bool IsEnabledBtnDoBooking()
+        {
+            if (_ViewModel == null)
+                return false;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo == null)
+                return false;
+            return wfInfo.InfoState > POPartslistInfoState.None;
+        }
+
+        private bool IsEnabledBtnOccupyMachine()
+        {
+            if (_ViewModel == null)
+                return false;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo == null)
+                return false;
+            return wfInfo.InfoState != POPartslistInfoState.Release;
+        }
+
+        private bool IsEnabledBtnReleaseMachine()
+        {
+            if (_ViewModel == null)
+                return false;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo == null)
+                return false;
+            return wfInfo.InfoState > POPartslistInfoState.None;
+        }
+
+        private bool IsEnabledBtnPauseOrderOnMachine()
+        {
+            if (_ViewModel == null)
+                return false;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo == null)
+                return false;
+            return wfInfo.InfoState > POPartslistInfoState.None;
+        }
+
+        private bool IsEnabledBtnEnterACMParams()
+        {
+            if (_ViewModel == null)
+                return false;
+            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (wfInfo == null)
+                return false;
+            return wfInfo.WFMethod != null;
+        }
+
+        #endregion
+
+
+    }
+}
--- gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/Views/BSOBarcode/BSOBarcodeTasksManu.xaml.cs
+++ gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/Views/BSOBarcode/BSOBarcodeTasksManu.xaml.cs
@@ -29,8 +29,8 @@ namespace gip.vb.mobile.Views
             base.OnAppearing();
             barcodeScanner._ViewModel = _ViewModel;
             barcodeScanner.OnAppearing();
-            EnableButtons();
-            SendChangedACMethod();
+            //EnableButtons();
+            //SendChangedACMethod();
             //if (_ViewModel != null)
             //    _ViewModel.ResetScanSequence();
         }
@@ -41,10 +41,10 @@ namespace gip.vb.mobile.Views
             base.OnDisappearing();
         }
 
-        private void barcodeScanner_OnBarcodeCommandInvoked(object sender, EventArgs e)
-        {
-            EnableButtons();
-        }
+        //private void barcodeScanner_OnBarcodeCommandInvoked(object sender, EventArgs e)
+        //{
+        //    EnableButtons();
+        //}
 
         private void CameraScanTBItem_Clicked(object sender, EventArgs e)
         {
@@ -52,141 +52,15 @@ namespace gip.vb.mobile.Views
             barcodeScanner.OpenCameraPanel();
         }
 
-        private void BtnReleaseMachine_Clicked(object sender, EventArgs e)
+
+
+        private async void barcodeScanner_OnSelectBarcodeEntity(object sender, EventArgs e)
         {
-            if (!IsEnabledBtnReleaseMachine())
-                return;
-
-            _ViewModel.ReleaseMachine();
-        }
-
-        private void BtnOccupyMachine_Clicked(object sender, EventArgs e)
-        {
-            if (!IsEnabledBtnOccupyMachine())
-                return;
-
-            _ViewModel.InvokeActionOnMachine();
-        }
-
-        private void BtnPauseOrderOnMachine_Clicked(object sender, EventArgs e)
-        {
-            if (!IsEnabledBtnPauseOrderOnMachine())
-                return;
-
-            _ViewModel.PauseOrderOnMachine();
-        }
-
-        private async void BtnDoBooking_Clicked(object sender, EventArgs e)
-        {
-            if (!IsEnabledBtnDoBooking())
-                return;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo.IntermediateBatch != null)
+            ProdOrderPartslistWFInfo selectedWfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
+            if (selectedWfInfo != null)
             {
-                if (!wfInfo.IntermediateBatch.IsFinalMixure)
-                {
-                    await Navigation.PushAsync(new BSOProdOrderOutwardMatSel(wfInfo.IntermediateBatch, _ViewModel));
-                }
-                else if (!wfInfo.IntermediateBatch.HasInputMaterials)
-                {
-                    await Navigation.PushAsync(new BSOProdOrderInward(wfInfo.IntermediateBatch, _ViewModel));
-                }
-                else
-                {
-                    await Navigation.PushAsync(new BSOProdOrderInOutSelector(wfInfo.IntermediateBatch, _ViewModel));
-                }
+                await Navigation.PushAsync(new BSOBarcodeTaskManuDetails(_ViewModel));
             }
-            else if (wfInfo.Intermediate != null)
-            {
-                await Navigation.PushAsync(new BSOProdOrderBatch(wfInfo.Intermediate, _ViewModel));
-            }
-            else
-            {
-                await Navigation.PushAsync(new BSOProdOrderIntermediate(wfInfo.ProdOrderPartslist, _ViewModel));
-            }
-        }
-
-        private async void BtnEnterACMParams_Clicked(object sender, EventArgs e)
-        {
-            if (!IsEnabledBtnEnterACMParams())
-                return;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo == null || wfInfo.WFMethod == null)
-                return;
-            wfInfo.WFMethod.AutoRemove = false;
-            await Navigation.PushAsync(new BSOACMethodEditor(wfInfo.WFMethod));
-        }
-
-        private void SendChangedACMethod()
-        {
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo != null && wfInfo.WFMethod != null && wfInfo.WFMethod.AutoRemove == true)
-            {
-                wfInfo.WFMethod.AutoRemove = false;
-                BarcodeEntity entity = _ViewModel.Item.Sequence.LastOrDefault();
-                if (entity == null)
-                {
-                    _ViewModel.ResetScanSequence();
-                    return;
-                }
-                entity.SelectedOrderWF = wfInfo;
-                entity.WFMethod = wfInfo.WFMethod;
-                _ViewModel.InvokeBarcodeCommand.Execute(null);
-            }
-        }
-
-        private void EnableButtons()
-        {
-            BtnDoBooking.IsEnabled = IsEnabledBtnDoBooking();
-            BtnOccupyMachine.IsVisible = IsEnabledBtnOccupyMachine();
-            BtnReleaseMachine.IsVisible = IsEnabledBtnReleaseMachine();
-            BtnPauseOrderOnMachine.IsVisible = IsEnabledBtnPauseOrderOnMachine();
-            BtnEnterACMParams.IsEnabled = IsEnabledBtnEnterACMParams();
-        }
-
-        private bool IsEnabledBtnDoBooking()
-        {
-            if (_ViewModel == null)
-                return false;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo == null)
-                return false;
-            return wfInfo.InfoState > POPartslistInfoState.None;
-        }
-
-        private bool IsEnabledBtnOccupyMachine()
-        {
-            if (_ViewModel == null)
-                return false;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo == null)
-                return false;
-            return wfInfo.InfoState != POPartslistInfoState.Release;
-        }
-
-        private bool IsEnabledBtnReleaseMachine()
-        {
-            if (_ViewModel == null)
-                return false;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo == null)
-                return false;
-            return wfInfo.InfoState > POPartslistInfoState.None;
-        }
-
-        private bool IsEnabledBtnPauseOrderOnMachine()
-        {
-            if (_ViewModel == null)
-                return false;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo == null)
-                return false;
-            return wfInfo.InfoState > POPartslistInfoState.None;
-        }
-
-        private void barcodeScanner_OnSelectBarcodeEntity(object sender, EventArgs e)
-        {
-            EnableButtons();
         }
 
         protected override bool OnBackButtonPressed()
@@ -195,15 +69,7 @@ namespace gip.vb.mobile.Views
             return true;
         }
 
-        private bool IsEnabledBtnEnterACMParams()
-        {
-            if (_ViewModel == null)
-                return false;
-            ProdOrderPartslistWFInfo wfInfo = _ViewModel.SelectedSequence as ProdOrderPartslistWFInfo;
-            if (wfInfo == null)
-                return false;
-            return wfInfo.WFMethod != null;
-        }
+
 
 
     }
