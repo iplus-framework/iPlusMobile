5237c47396fde66e96e4f6d9624db6e3956da610
Ivan Hrastinski <iveksp@gmail.com>
2025-03-07 18:34:39 +0100

    bugfix

--- gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/ViewModels/Facility/FacilityChargesViewModel.cs
+++ gip.vb.mobile/gip.vb.mobile/gip.vb.mobile/ViewModels/Facility/FacilityChargesViewModel.cs
@@ -653,7 +653,10 @@ namespace gip.vb.mobile.ViewModels
                 aCMethodBooking.VirtualMethodName = gip.mes.datamodel.GlobalApp.FBT_Split_FacilityCharge.ToString();
                 aCMethodBooking.OutwardFacilityChargeID = Item.FacilityChargeID;
                 aCMethodBooking.OutwardQuantity = BookingQuantity;
-                aCMethodBooking.InwardSplitNo = splitNo;
+                if (splitNo == 0)
+                    aCMethodBooking.InwardAutoSplitQuant = 1;
+                else
+                    aCMethodBooking.InwardSplitNo = splitNo;
 
                 WSResponse<MsgWithDetails> response = await _WebService.BookFacilityAsync(aCMethodBooking);
 
